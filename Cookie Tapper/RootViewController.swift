9fd2c42ef289e6bba93ba3190b173d107b584043
Naina <Naina@Nainas-Macbook-Air.local>
2017-09-26 09:54:07 -0400

    Initial Commit

---
+++ Cookie Tapper/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  Cookie Tapper
+//
+//  Created by Naina  on 9/26/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
