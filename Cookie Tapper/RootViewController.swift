8a1b6e725b8552f49a7ac8f47306e57fbd85f210
Naina <nainanoorali@gmail.com>
2019-03-04 21:13:19 -0500

    Initial commit
    IOS development bootcamp code

--- Cookie Tapper/RootViewController.swift
+++ Cookie Tapper/RootViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class RootViewController: UIViewController {
 
+    @IBOutlet weak var tapGoalLabel: UILabel!
+    @IBOutlet weak var tapCounterLabel: UILabel!
+    var currentTapCount = 0
+    let tapGoal = 10
+    var isTapping = false
+    var goalIsMet:Bool {
+        return currentTapCount >= tapGoal}
+    var tapOrTaps: String {
+         if currentTapCount != 1{
+            return "taps"
+        } else {
+            return "Tap"
+            }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tapGoalLabel.text = " Goal: \(tapGoal)"
+        updateTapCountLable()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onCookieTapped(_ sender: Any) {
+        if !goalIsMet {
+            isTapping = true
+            incrementTapCount()
+            updateTapCountLable()
+        }
+        
+    }
+    func incrementTapCount() {
+        currentTapCount += 1
+        if goalIsMet {
+            isTapping = false
+        }
+        
+    }
+    func updateTapCountLable() {
+        
+        
+            if isTapping  {
+                tapCounterLabel.textColor = UIColor.blue
+            }
+                else {
+                     tapCounterLabel.textColor = UIColor.black
+                }
+            
+        tapCounterLabel.text = " \(currentTapCount) " + tapOrTaps
+        }
 }
 
